0970156d67217d8b897ad34f6ad1dd494ee77271
Qiride <3124899838@qq.com>
2025-02-14 15:08:19 +0800

    Site updated: 2025-02-14 15:08:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["post/21ba1eea.html","post/a1286495.html","post/e9c05029.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["post/21ba1eea.html","post/a1286495.html","post/e9c05029.html","post/d2407e80.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
